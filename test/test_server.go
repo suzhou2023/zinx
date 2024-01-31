ab08cb2eef39d6925dfff58453d3aed62e2484a1
suzhou <withwind318@gmail.com>
2024-01-31 08:40:13 +0800

    init

---
+++ test/test_server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"zinx/ziface"
+	"zinx/znet"
+)
+
+// ping test 自定义路由
+type PingRouter struct {
+	znet.BaseRouter //一定要先基础BaseRouter
+}
+
+// Handle
+func (this *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call PingRouter Handle")
+	//先读取客户端的数据，再回写ping...ping...ping
+	fmt.Println("recv from client : msgId=", request.GetMsgId(), ", data=", string(request.GetData()))
+
+	//回写数据
+	err := request.GetConnection().SendMsg(1, []byte("ping...ping...ping"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// PreHandle
+func (this *PingRouter) PreHandle(request ziface.IRequest) {
+	fmt.Println("Call Router PreHandle")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping ....\n"))
+	if err != nil {
+		fmt.Println("call back ping ping ping error")
+	}
+}
+
+// PostHandle
+func (this *PingRouter) PostHandle(request ziface.IRequest) {
+	fmt.Println("Call Router PostHandle")
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("After ping .....\n"))
+	if err != nil {
+		fmt.Println("call back ping ping ping error")
+	}
+}
+
+func main() {
+	//创建一个server句柄
+	s := znet.NewServer()
+
+	s.AddRouter(&PingRouter{})
+
+	//2 开启服务
+	s.Serve()
+}
---
+++ ziface/iconnection.go
@@ -0,0 +1,27 @@
+package ziface
+
+import "net"
+
+// 定义连接接⼝
+type IConnection interface {
+	// 启动连接，让当前连接开始⼯工作
+	Start()
+
+	// 停⽌止连接，结束当前连接状态M
+	Stop()
+
+	// 从当前连接获取原始的socket TCPConn
+	GetTCPConnection() *net.TCPConn
+
+	//获取当前连接ID
+	GetConnID() uint32
+
+	//获取远程客户端地址信息
+	RemoteAddr() net.Addr
+
+	//直接将Message数据发送数据给远程的TCP客户端
+	SendMsg(msgId uint32, data []byte) error
+}
+
+// 定义⼀一个统⼀一处理理链接业务的接⼝口
+type HandFunc func(*net.TCPConn, []byte, int) error
